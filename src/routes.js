d3c1f489fd31f706cb3935c800c820fa0316c103
jjullimayanne <julli.souza@ufob.edu.br>
2021-11-09 12:29:24 -0300

    working

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import React from "react";
 ////import { Route, Redirect}from 'react-router';
 import { Switch, Route } from "react-router-dom";
 import WebcamCapture from "../src/pages/auth";
+import QRscan from "./pages/qrscanner/QRscanner";
 
-export default (props) => (
+export default () => (
     <Switch>
-      <Route exact path="/" component={WebcamCapture} />
+      <Route exact path="/cam" component={WebcamCapture} />
+      <Route exact path="/qr" component={QRscan} />
     </Switch>
 );
