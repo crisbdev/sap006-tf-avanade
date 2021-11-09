e24e07afc50f9b3a21bf606da6079e9969fbab2a
jjullimayanne <julli.souza@ufob.edu.br>
2021-11-09 19:33:18 -0300

    qr code lendo

--- src/pages/qrscanner/QRscanner.js
+++ src/pages/qrscanner/QRscanner.js
@@ -1,35 +1,78 @@
 /* eslint-disable  */
 /* ewline required at end of file but not found */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import QrScan from 'react-qr-reader';
 import './Qrscanner.css';
 import firebase from 'firebase/compat/app';
+import 'firebase/firestore';
 import dataStore from '../../services/firebaseConfig';
 
 function QRscanner() {
+ /// const ref = dataStore().collection("tokens")
+ const ref = firebase.firestore().collection("tokens")
+  ///console.log(ref)
+  const [token, setToken] = useState([])
+
+  function getToken() {
+    ref.onSnapshot((QuerySnapshot) => {
+      const items = [];
+      QuerySnapshot.forEach((doc) => {
+        items.push(doc.data());
+
+      }); 
+      setToken(items)
+    })
+  }
+  useEffect(() => {
+    getToken();
+  }, []);
+
+
+//  // ref.get().then((snap) => {
+//   snap.forEach((post)=> {
+//     if (post.data().status ===("avaliable"){
+//       console.log("wdff")
+//     }else{
+//       console.log("svf")
+//     }
+///       {token.map((token) => (
+ //// <h1>{token.tittle}</h1>
+ //// ))}
+
+
   const [qrscan, setQrscan] = useState('No result');
+
   ///console.log(qrscan);
   const handleScan = (data) => {
+    
     setQrscan(data);
-    console.log(data);
 
-    if (data) {
-      setQrscan(data);  
-    }
+   const dataprint = data; 
+   console.log(dataprint)
+   ref.get().then((snap) => {
+    snap.forEach((post) => {
+      if (post.data().status === dataprint){
+      alert("VACINADO")
+      } else {
+    
+      }
+    })
+  })
+
   };
+
   const handleError = (err) => {
-    ////console.error(err);
+    console.error(err);
   };
   
-  const addPost = (texto) => {
-    dataStore().collection('posts').add({
-      text: texto,
-    });
-  };
-
   return (
     <div>
+          {qrscan ? <h1>Li o qr (dado)</h1> : false}
       <div>
+        {token.map((token) => (
+          <h1>{token.title}</h1>
+        ))}
+     
         <QrScan
           delay={300}
           onError={handleError}
